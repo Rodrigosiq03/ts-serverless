5fb945fe358065e9b9545e28dd0f2abb5536e1de
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-08-07 11:15:44 -0300

    added delete-user route👽✔

---
+++ src/handlers/deleteUser.ts
@@ -0,0 +1,28 @@
+import { client } from "../db/client";
+import { DeleteItemCommand } from "@aws-sdk/client-dynamodb"
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
+
+export async function getUser(event: any) {
+  const response = { statusCode: 200, body: '' };
+
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: marshall({ userId: event.queryStringParameters.userId })
+    }
+    const _ = await client.send(new DeleteItemCommand(params));
+    response.body = JSON.stringify({
+      message: "Successfully deleted user",
+    });
+  } catch (err) {
+    console.error(err);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to delete user",
+      error: err.message,
+      errorStack: err.stack
+    });
+  }
+
+  return response;
+}
